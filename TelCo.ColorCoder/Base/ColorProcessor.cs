51a47d9a5140354bc9362370424c645d3adb72ea
QOH2KOR <QOH2KOR@BMHE1115826>
2022-11-22 18:52:27 +0530

    Initial Commit

---
+++ TelCo.ColorCoder/Base/ColorProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TelCo.ColorCoder.Interface;
+using TelCo.ColorCoder.UnitTestAssertion;
+
+namespace TelCo.ColorCoder
+{
+    public class ColorProcessor
+    {
+        private ColorPair _pair;
+        private int _pairNumber, minorSize;
+        public ColorProcessor(ColorPair pair = null, int pairNumber = 0)
+        {
+            _pair = pair;
+            _pairNumber = pairNumber;
+        }
+        public void GetPairNumberFromColorProcessor()
+        {
+            int majorIndex = ColorCalculationForPairNumberFromColor.GetMajorColor(_pair);
+            int minorIndex = ColorCalculationForPairNumberFromColor.GetMinorColor(_pair);
+            int result = EvaluateColor.EvaluateColorPairNumber(_pair, majorIndex, minorIndex);
+            Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", _pair, result);
+            IUnitTestForColor unitTestForColor = new PairNumberFromColorAssertion(_pair, result);
+            unitTestForColor.AssertionCheck();
+        }
+        public void GetColorFromPairNumberProcessor()
+        {
+            minorSize = EvaluateColor.EvaluateIsValidPairNumber(_pairNumber);
+            ColorPair colorPair = ColorCalculationForColorFromPairNumber.GetcolorPairNumber(_pairNumber, minorSize);
+            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", _pairNumber, colorPair);
+            IUnitTestForColor unitTestForColor = new ColorFromPairNumberAssertion(colorPair, _pairNumber);
+            unitTestForColor.AssertionCheck();
+        }
+    }
+}
---
+++ TelCo.ColorCoder/UnitTestAssertion/ColorFromPairNumberAssertion.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using TelCo.ColorCoder.Interface;
+
+namespace TelCo.ColorCoder.UnitTestAssertion
+{
+    public class ColorFromPairNumberAssertion : IUnitTestForColor
+    {
+        private ColorPair _colorPair;
+        private int _pairNumber;
+        public ColorFromPairNumberAssertion(ColorPair colorPair, int pairNumber)
+        {
+            _colorPair = colorPair;
+            _pairNumber = pairNumber;
+        }
+        public void AssertionCheck()
+        {
+            switch (_pairNumber)
+            {
+                case 4:
+                    Debug.Assert(_colorPair.majorColor == Color.White);
+                    Debug.Assert(_colorPair.minorColor == Color.Brown);
+                    break;
+                case 5:
+                    Debug.Assert(_colorPair.majorColor == Color.White);
+                    Debug.Assert(_colorPair.minorColor == Color.SlateGray);
+                    break;
+                case 34:
+                    Debug.Assert(_colorPair.majorColor == Color.Violet);
+                    Debug.Assert(_colorPair.minorColor == Color.Green);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
